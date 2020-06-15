dd0d2f88e9737c57ee6712b7203769e09e107623
dorinereijersen <45545352+dorinereijersen@users.noreply.github.com>
2020-06-15 13:16:26 +0200

    First upload

---
+++ text/typed.js
@@ -0,0 +1,43 @@
+/* eslint-env es6 */
+/* eslint-disable no-console */
+
+/* eslint-env es6 */
+/* eslint-disable */
+
+//$.FullScreen(
+//move: '.wrapper'),
+//row: '.row',
+//nav: '.nav',
+//time: '1s',
+//type:'cubic-bezier(1,-0.12,0.44,0.99)',
+//navclass:'cursor'
+//
+//minHeight: 200;
+//
+
+
+//const buttons = document.getElementsByTagName("button");
+//
+//for (const button of buttons) {
+//  button.addEventListener('click', () => {
+//     var id = button.getAttribute("id");
+//    
+//     var layerClass = "." + id + "-layer";
+//     var layers = document.querySelectorAll(layerClass);
+//     for (const layer of layers) {
+//       layer.classList.toggle("active");
+//     }
+//  });
+//}
+
+/*eslint-env browser*/
+//document.getElementById("calltoaction").addEventListener("click", fire);
+//
+//function fire() {
+//    console.log('clicked');
+//    document.getElementById("contentBox").remove();
+//    document.getElementById("animateGif").play();
+//}
+    AOS.init( {
+        duration: 1000
+    });
